4401d232812120126668d552092eeaf62bbde778
aishwarya <ashp17star@gmail.com>
2022-12-16 17:07:43 +0530

    retrieve, update, delete operations on Core Data

--- 03_10_2022_CoreDataDemo/ViewController.swift
+++ 03_10_2022_CoreDataDemo/ViewController.swift
@@ -14,7 +14,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        insertData()
+        //insertData()
+        
+        retriveEmployeeRecords()
+        print("================")
+        //updateEmployeeRecord()
+        retriveEmployeeRecords()
+        print("================")
+        deleteEmployeeRecord()
+        retriveEmployeeRecords()
+        print("================")
+        
     }
     
     func insertData(){
@@ -40,4 +50,70 @@ class ViewController: UIViewController {
         }
     }
     
+    func retriveEmployeeRecords(){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest : NSFetchRequest<NSFetchRequestResult> = NSFetchRequest<NSFetchRequestResult>(entityName: "Employee")
+        
+        do{
+            let fetchResult = try managedContext.fetch(fetchRequest)
+            
+            for eachFetchResult in fetchResult as! [NSManagedObject]{
+                print("The employee record \(eachFetchResult.value(forKey: "empName"))")
+            }
+        } catch {
+            print("Failed to extract Employee Records")
+        }
+    }
+    
+    func updateEmployeeRecord(){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequestQueryForUpdate : NSFetchRequest<NSFetchRequestResult> =
+        NSFetchRequest<NSFetchRequestResult>.init(entityName: "Employee")
+        
+        fetchRequestQueryForUpdate.predicate = NSPredicate(format: "empName = %@","Employee -1")
+        
+        do{
+            let employeeObjects = try managedContext.fetch(fetchRequestQueryForUpdate)
+            let employeeObject = employeeObjects[0] as! NSManagedObject
+            employeeObject.setValue("Employee - 4", forKey: "empName")
+            employeeObject.setValue(4, forKey: "empId")
+            employeeObject.setValue(27004.23, forKey: "empSalary")
+        } catch {
+            print("Failed to update Employee Record")
+        }
+    }
+    
+    func deleteEmployeeRecord(){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequestForDelete : NSFetchRequest<NSFetchRequestResult> =
+        NSFetchRequest<NSFetchRequestResult>(entityName: "Employee")
+        
+        fetchRequestForDelete.predicate = NSPredicate(format: "empName = %@","Employee -3")
+        do{
+            let employees = try managedContext.fetch(fetchRequestForDelete)
+            let employeeRecordToBeDeleted = employees[0] as! NSManagedObject
+            managedContext.delete(employeeRecordToBeDeleted)
+            
+            do{
+                try managedContext.save()
+            } catch {
+                print("error")
+            }
+            
+        } catch {
+            print("Employee Record Deletion Falied")
+        }
+    }
 }
